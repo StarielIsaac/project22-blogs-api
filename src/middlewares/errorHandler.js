cfb8c7145697c70cb621919e1d11dd05db1c1cfe
Stariel Isaac <starielisaac@gmail.com>
2023-04-11 17:20:43 -0300

    Refactoring: middleware - errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -7,14 +7,9 @@ module.exports = (error, _req, res, _next) => {
   if (error instanceof ErrorLaunch) {
     return res.status(error.code).json({ message: error.message });
   }
-  if (error instanceof TokenExpiredError) {
+  if (error instanceof TokenExpiredError || error instanceof JsonWebTokenError) {
     return res.status(401).json({ message: 'Expired or invalid token' });
-  }
-
-  if (error instanceof JsonWebTokenError) {
-      return res.status(401).json({ message: 'Expired or invalid token' });
-  }
-  
+}
   if (error.status) return res.status(error.status).json({ message: error.message });
   
   return res.status(500).json({ message: 'internal server error' });
