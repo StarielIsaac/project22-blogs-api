23a7ce64e44cad6fb2664f9403c42571dfd92c67
Stariel Isaac <starielisaac@gmail.com>
2023-04-11 16:08:21 -0300

    feature: created router/ controller/ service/ model - user

--- src/middlewares/validadeUser.js
+++ src/middlewares/validadeUser.js
@@ -3,17 +3,17 @@ const errorMensage = require('../utils/errorMensage');
 const validadeUser = (req, res, next) => {
   const { displayName, email, password } = req.body;
   if (displayName.length < 8) {
-    throw errorMensage(400, 'displayName length must be at least 8 characters long');
+    throw errorMensage(400, '"displayName" length must be at least 8 characters long');
   }
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const isValed = regex.test(email);  
 
   if (!isValed) {
-    throw errorMensage(400, 'email must be a valid email');
+    throw errorMensage(400, '"email" must be a valid email');
   }
 
   if (password.length < 6) {
-    throw errorMensage(400, 'password length must be at least 6 characters long');
+    throw errorMensage(400, '"password" length must be at least 6 characters long');
   }
 
   next();
--- src/service/userService.js
+++ src/service/userService.js
@@ -1,15 +1,22 @@
 // const ErrorLaunch = require('../utils/errorHandle');
-const { User } = require('../models/User');
+const { User } = require('../models');
+const ErrorLaunch = require('../utils/errorHandle');
 const { createToken } = require('../utils/auth');
 
-const createUser = async (info) => {
 // chama a model (sql)
-  
+const createUser = async (info) => {
+const { email } = info;
+// verifica se o usuario existe
+const userExist = await User.findOne({ where: { email } });
+if (userExist) {
+  // console.log('ja existe essa conta');
+  throw new ErrorLaunch('User already registered', 409);
+}
 // adiciona novo usuario no db
-await User.create(info);
+const newUser = await User.create(info);
 
 // cria um token
-const token = createToken(info);
+const token = createToken({ id: newUser.id });
 
 return { token };
 };
