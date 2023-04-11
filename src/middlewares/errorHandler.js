7f2e00f9db4b54944f794b8f024c16d72c0b5e87
Stariel Isaac <starielisaac@gmail.com>
2023-04-10 21:36:56 -0300

    tentando req 03

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,13 @@
 // se existir error.status retorna um erro com status 500 com a mensagem do erro, se não
 // retorna apenas um erro genérico
+const ErrorLaunch = require('../utils/errorHandle');
+
 module.exports = (error, _req, res, _next) => {
-    if (error.status) return res.status(error.status).json({ message: error.message });
-    return res.status(500).json({ message: 'internal server error' });
+  if (error instanceof ErrorLaunch) {
+    return res.status(error.code).json({ message: error.message });
+  }
+  
+  if (error.status) return res.status(error.status).json({ message: error.message });
+  
+  return res.status(500).json({ message: 'internal server error' });
   };
--- src/service/loginService.js
+++ src/service/loginService.js
@@ -1,14 +1,14 @@
 const { User } = require('../models/User');
-const errorMensage = require('../middlewares/errorHandler');
 const { createToken } = require('../utils/auth');
+const ErrorLaunch = require('../utils/errorHandle');
 
 // funcao que verifica se o token existe, se sim cria um token e retorna-o
 const findUser = async (email, password) => {
     const user = await User.findOne({ where: { email, password } });
     // console.log(user);
 
-    if (!user) {    
-       throw errorMensage(400, 'Invalid fields');
+    if (!user) {
+        throw new ErrorLaunch('Invalid fields', 400);
     }
 
     const token = createToken({ id: user.id });
