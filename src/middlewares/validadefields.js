13bae7d52cb7a0de58b7fef28bc25dc5c9fb9bd8
Stariel Isaac <starielisaac@gmail.com>
2023-04-26 17:30:56 -0300

    fix: destructuring - user.id

--- src/middlewares/validadefields.js
+++ src/middlewares/validadefields.js
@@ -1,5 +1,6 @@
 const errorMensage = require('../utils/errorMensage');
 
+// valida o title e o content que bem de req.body
 const validadePost = async (req, res, next) => {
   const { title, content } = req.body;
 
